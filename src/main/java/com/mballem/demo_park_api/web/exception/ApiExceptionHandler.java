7d55fb8dc04d4ca211913deb4ca7941659575f4a
LorenzoFer <rafael.silva@ibrowse-sds.com.br>
2025-02-20 13:23:58 -0300

    Criado o package Exception para tratar as exeções do sistema, criado as classes ErrorMessage ainda nã sei muito bem para que serve, e a classe ApiExceptionHandler que é pra capturar as exeções Http e tratalas de alguma forma, fiz uns comeéntarios para me orientar melhor nela

---
+++ src/main/java/com/mballem/demo_park_api/web/exception/ApiExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.mballem.demo_park_api.web.exception;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    // Esse ExceptionHandler vai pegar somente as exeções de MethodArgumentNotValidException e vai tratar esse tipo de exeção
+    // ResponseEntity é do própio Spring ele permite personalizar as respostar Http
+    // Na assinatura do metodo tem o erro que foi capturado pelo Handler (ex), qual requisição Http do erro(request) e quais campos falharam(result)
+    // StatusUnprocessable_entity(422) o servidor entendeu a requisição porem dados enviados estão errados.
+    // ContentType define o tipo de resposta no caso dessa função será JSON
+    // O Body define o corpo de resposta no caso sera uma menssagem de erro
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessage> MethodArgumentNotValidException(MethodArgumentNotValidException ex,
+                                                                        HttpServletRequest request,
+                                                                        BindingResult result){
+        log.error("Api Error - ", ex);
+        return ResponseEntity
+                .status(HttpStatus.UNPROCESSABLE_ENTITY)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.UNPROCESSABLE_ENTITY, "Campo(s) invalido(s)", result));
+
+    }
+}
