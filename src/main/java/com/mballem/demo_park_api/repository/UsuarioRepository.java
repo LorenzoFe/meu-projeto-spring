f7525629ce664745bbfc89d40467ab2c5d562f50
LorenzoFer <rafael.silva@ibrowse-sds.com.br>
2025-05-13 11:54:06 -0300

    feito toda a configuração do spring security e Jwt até o momento está funcionando o cadastro (senha criptografada), autenticar o usuário e o end point de buscarTodos

--- src/main/java/com/mballem/demo_park_api/repository/UsuarioRepository.java
+++ src/main/java/com/mballem/demo_park_api/repository/UsuarioRepository.java
@@ -2,6 +2,14 @@ package com.mballem.demo_park_api.repository;
 
 import com.mballem.demo_park_api.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByUsername(String username);
+
+    @Query("select u.role from Usuario u where u.username = :username")
+    Usuario.Role findRoleByUsername(@Param("username") String username);
 }
